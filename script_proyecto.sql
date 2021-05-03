87a434df76e23c2a20b456d19b4c319fe9e9aca1
appajimenez <83054696+appajimenez@users.noreply.github.com>
2021-05-03 18:25:36 -0500

    Update script_proyecto.sql

--- script_proyecto.sql
+++ script_proyecto.sql
@@ -1,9 +1,21 @@
+USE master
+GO
+-- VALIDACION BD_RESERVA
+IF DB_ID('BD_RESERVA') IS NOT NULL
+DROP DATABASE BD_RESERVA
+GO
+
+-- CREACION BD_RESERVA
 CREATE DATABASE BD_RESERVA
 GO
 
 USE BD_RESERVA
 GO
 
+-- TABLA SEDE
+IF OBJECT_ID('sede') IS NOT NULL
+DROP TABLE sede
+go
 CREATE TABLE sede
 (
 id_sede int identity(1,1) primary key,
@@ -13,13 +25,23 @@ direc_sede varchar(200),
 distrito_sede varchar(30),
 provincia varchar(30)
 )
+GO
 
+-- TABLA ESPECIALIDAD
+IF OBJECT_ID('especialidad') IS NOT NULL
+DROP TABLE especialidad
+go
 CREATE TABLE especialidad
 (
 id_especialidad int identity(1,1) primary key,
 descrip_especialidad varchar(30),
 )
+GO
 
+-- TABLA DOCTOR
+IF OBJECT_ID('doctor') IS NOT NULL
+DROP TABLE doctor
+go
 CREATE TABLE doctor
 (
 id_doctor int identity(1,1) primary key,
@@ -35,6 +57,7 @@ estado_doctor bit,
 id_especialidad int,
 id_sede int
 )
+GO
 
 ALTER TABLE doctor ADD CONSTRAINT fk_especialidad_doctor
 FOREIGN KEY(id_especialidad) REFERENCES especialidad(id_especialidad)
@@ -42,6 +65,10 @@ FOREIGN KEY(id_especialidad) REFERENCES especialidad(id_especialidad)
 ALTER TABLE doctor ADD CONSTRAINT fk_sede_doctor
 FOREIGN KEY(id_sede) REFERENCES sede(id_sede)
 
+-- TABLA PACIENTE
+IF OBJECT_ID('paciente') IS NOT NULL
+DROP TABLE paciente
+go
 CREATE TABLE paciente
 (
 id_paciente int identity(1,1) primary key,
@@ -54,20 +81,35 @@ tel_paciente varchar(20),
 email_paciente varchar(100),
 grup_sang varchar(4)
 )
+GO
 
+-- TABLA TURNO
+IF OBJECT_ID('turno') IS NOT NULL
+DROP TABLE turno
+go
 CREATE TABLE turno
 (
 id_turno int identity(1,1) primary key,
 nom_turno varchar(30),
 descrip_turno varchar(30)
 )
+GO
 
+-- TABLA METODO PAGO
+IF OBJECT_ID('metodo_pago') IS NOT NULL
+DROP TABLE metodo_pago
+go
 CREATE TABLE metodo_pago
 (
 id_pago int identity(1,1) primary key,
 tipo_pago varchar(30)
 )
+GO
 
+-- TABLA RESERVA
+IF OBJECT_ID('reserva') IS NOT NULL
+DROP TABLE reserva
+go
 CREATE TABLE reserva
 (
 id_reserva int identity(1,1) primary key,
@@ -84,6 +126,7 @@ id_especialidad int,
 id_turno int,
 id_pago int,
 )
+GO
 
 ALTER TABLE reserva ADD CONSTRAINT fk_sede_reserva
 FOREIGN KEY(id_sede) REFERENCES sede(id_sede)
@@ -103,6 +146,10 @@ FOREIGN KEY(id_turno) REFERENCES turno(id_turno)
 ALTER TABLE reserva ADD CONSTRAINT fk_pago_reserva
 FOREIGN KEY(id_pago) REFERENCES metodo_pago(id_pago)
 
+-- TABLA USUARIO
+IF OBJECT_ID('usuario') IS NOT NULL
+DROP TABLE usuario
+go
 CREATE TABLE usuario
 (
 id_usuario int identity(1,1) primary key,
@@ -115,3 +162,4 @@ tel_usuario int,
 email_usuario varchar(100),
 estado_usuario bit
 )
+GO
